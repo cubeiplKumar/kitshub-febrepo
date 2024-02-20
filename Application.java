96fdf76d98fe6147f3844869c58c1e976e4eb975
cubeiplkumar <emailanilkumarm@gmail.com>
2024-02-20 12:06:18 +0530

    Application.java file modified by Ethan

--- Application.java
+++ Application.java
@@ -38,6 +38,26 @@ public class FreqTableExampleOriginal {
         catch(IOException e){
             System.out.println("Problem while reading from file. Unable to create freq table" + e);
         }
+		#feature2.1 changes by Ethan for the Savings Module in Oct 2023
+		 // create a method
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+    // return value
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    
+    int num1 = 25;
+    int num2 = 15;
+
+    // create an object of Main
+    Main obj = new Main();
+    // calling method
+    int result = obj.addNumbers(num1, num2);
+    System.out.println("Sum is: " + result);
+  }
+}
         if( freqs.length == 0)
             System.out.println("No frequency table created due to problems when reading from file");
         else{
